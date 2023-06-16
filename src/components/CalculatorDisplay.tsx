1738aa917f1fcef43937836e93a2c20f96f5cd6c
stefaleon <stefanidisl@iconplatforms.com>
2023-06-16 14:59:15 +0300

    Copy displayed value to clipboard

--- src/components/CalculatorDisplay.tsx
+++ src/components/CalculatorDisplay.tsx
@@ -1,9 +1,40 @@
+import { useRef } from "react";
 import { useCalculator } from "../context/useCalculator";
 
 const CalculatorDisplay: React.FC = () => {
 	const { value } = useCalculator();
+	const divRef = useRef<HTMLDivElement>(null);
 
-	return <div>{value}</div>;
+	// const handleCopy = () => {
+	// 	if (divRef.current) {
+	// 		const range = document.createRange();
+	// 		range.selectNode(divRef.current);
+	// 		window.getSelection()?.removeAllRanges();
+	// 		window.getSelection()?.addRange(range);
+	// 		document.execCommand("copy");
+	// 		window.getSelection()?.removeAllRanges();
+	// 	}
+	// };
+	
+	const handleCopy = () => {
+		if (divRef.current) {
+			const textToCopy = divRef.current.innerText;
+			navigator.clipboard
+				.writeText(textToCopy)
+				.then(() => {
+					console.log("Text copied to clipboard");
+				})
+				.catch((error) => {
+					console.error("Failed to copy text to clipboard:", error);
+				});
+		}
+	};
+
+	return (
+		<div ref={divRef} onClick={handleCopy}>
+			{value}
+		</div>
+	);
 };
 
 export default CalculatorDisplay;
